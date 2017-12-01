fe67e32343aaebdb15cba5c7bca820f0544e4410
Leo <zenlionel@gmail.com>
2017-12-01 08:21:50 -0800

    this is not a finished product

---
+++ bamazon.sql
@@ -0,0 +1,16 @@
+DROP DATABASE IF EXISTS bamazonDB;
+CREATE database bamazonDB;
+
+USE bamazonDB;
+
+CREATE TABLE products (
+    item_id INT(4) NOT NULL;
+    product_name VARCHAR(40) NOT NULL;
+    department_name VARCHAR(40) NOT NULL;
+    price DECIMAL(10,2) NOT NULL;
+    stock_quantity INT(3) NOT NULL;
+    PRIMARY KEY(item_id);
+);
+
+INSERT INTO products (item_id, product_name, department_name, price, stock_quantity)
+VALUES(0001, 'chocolate', 'food', 3.20, 30),(0002,'vanilla','food',3.10,20),(0003,'nail','hardware',0.50,10),(0004,'hammer','hardware',4.00,10),(0005,'table','hardware',20.00, 5),(0006,'PC','electronics',1000.00,4)(0007,'Mac','electronics',1200.00,4)
---
+++ bamazoncustomer.js
@@ -0,0 +1,122 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "3Nkerman",
+    database: "bamazonDB"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId + "\n");
+    start();
+});
+
+
+function start() {
+
+    inquirer
+        .prompt([{
+            type: "list",
+            message: "What would you like to do?",
+            choices: ["See List", "Buy", "End"],
+            name: "choice"
+        }])
+        .then(function (answer) {
+            if (answer.choice === "See List") {
+                //do post
+                showInfo();
+            } else if (answer.choice === "Buy") {
+                buyItem();
+            } else if (answer.choice === "End") {
+                console.log("bye");
+                connection.end();
+            }
+        })
+        .catch(function (err) {
+            console.log(err);
+        })
+}
+
+function showInfo() {
+    inquirer
+        .prompt({
+            name: "products",
+            type: "confirm",
+            message: "Are you sure you would like to see detailed item list?"
+        })
+        .then(function (answer) {
+            var query = "SELECT * FROM products";
+            connection.query(query, function (err, res) {
+                if (err) throw err
+                for (var i = 0; i < res.length; i++) {
+                    console.log("Product ID: " + res[i].item_id + " || Product Name: " + res[i].product_name + " || Department: " + res[i].department_name + " || Price: " + res[i].price + " || Stock: " + res[i].stock_quantity);
+                    console.log('------------------------------');
+                }
+                start();
+            });
+        });
+}
+
+function buyItem() {
+    
+    connection.query("SELECT * FROM products", function (err, results) {
+        if (err) throw err;
+       
+        inquirer
+            .prompt([{
+                    name: "Product choice",
+                    type: "rawlist",
+                    choices: function () {
+                        var choiceArray = [];
+                        for (var i = 0; i < results.length; i++) {
+                            choiceArray.push(results[i].product_name);
+                        }
+                        return choiceArray;
+                    },
+                    message: "What item would you like to buy?"
+                },
+                {
+                    name: "buy",
+                    type: "input",
+                    message: "How much would you like to buy?"
+                }
+            ])
+            .then(function (answer) {
+                var chosenItem;
+                for (var i = 0; i < results.length; i++) {
+                    if (results[i].product_name === answer.choice) {
+                        chosenItem = results[i];
+                    }
+                }
+                if (chosenItem.buy < parseInt(answer.stock)) {
+                    
+                    connection.query(
+                        "UPDATE stock_quantity SET ? WHERE ?", [{
+                                stock_quantity: answer.buy
+                            },
+                            {
+                                product_id: chosenItem.product_id
+                            }
+                        ],
+                        function (error) {
+                            if (error) throw err;
+                            console.log("Bought succesfully");
+                            start();
+                        }
+                    );
+                } else {
+                    console.log("We don't have enough in stock!");
+                    start();
+                }
+            });
+    });
+}
+//I got lost in my code :( I don't like inquirer...
